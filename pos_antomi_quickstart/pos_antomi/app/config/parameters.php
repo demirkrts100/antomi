6c7311703889c3fa69d763a5587b16e11bb4f357
unknown <demirkrts12@gmail.com>
2025-04-12 11:53:59 +0300

    fist commit

---
+++ pos_antomi_quickstart/pos_antomi/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'pos_antomi_demo2',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'fZyKTAxzHKU1qxi6LUE7eI5mGJO170sEuZJUJMC0Hg4dDyTa9C2dFxxe',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2019-06-20',
+    'locale' => 'en-US',
+    'cookie_key' => 'JFG92frSvLYONKxFViSPLDH5sE0JJqZVwyS1vWHjNCTKsA3fvuAjkzEY',
+    'cookie_iv' => 'suHn8QRO',
+    'new_cookie_key' => 'def00000809db028c3a025a3552a46ca2d662ee98515303a2de35bb52cc928a0334e034bebe7505718d37edcbd9e40bf2445957bb637ef56660e4d1d04c0b9bb112f4c2a',
+  ),
+);
---
+++ readme.txt
@@ -0,0 +1,8 @@
+04/02/2023: Update version 1.0.3
+	- Updated theme compatible with Prestashop 8
+
+03/13/2022: Update version 1.0.2
+	- Updated theme compatible with Prestashop 1.7.8.4
+
+12/02/2021: Update version 1.0.1
+	- Updated theme compatible with Prestashop 1.7.8.0
